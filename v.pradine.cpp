f131368a1e8f08485cb79adb7faf1e552fc3a855
ArnoldasViduto <vidutoarnas@gmail.com>
2026-09-24 14:19:39 +0300

    Studentu medianos pridejimas
    Skaiciuojamas ne tik vidurkis bet ir mediana

--- v.pradine.cpp
+++ v.pradine.cpp
@@ -4,12 +4,14 @@
 #include <sstream>
 #include <string>
 #include <vector>
+#include <algorithm>
 
 struct studentas {
     std::string var, pav;
     std::vector<int> paz;
     int egz;
-    double rez;
+    double vid;
+    double med;
 };
 
 int main()
@@ -36,20 +38,48 @@ int main()
             f >> A.paz[i];
         f >> A.egz;
 
-        int suma = A.egz;
-        for (int p : A.paz) suma += p;
-        A.rez = (double)suma / (ndKiek + 1);
+        vector<int> visi = A.paz;
+        visi.push_back(A.egz);
+
+        int suma = 0;
+        for (int p : visi) suma += p;
+        A.vid = (double)suma / visi.size();
+
+        sort(visi.begin(), visi.end());
+        int n = visi.size();
+        if (n % 2 == 0)
+            A.med = (visi[n / 2 - 1] + visi[n / 2]) / 2.0;
+        else
+            A.med = visi[n / 2];
 
         studentai.push_back(A);
     }
 
-    cout << left << setw(16) << "Pavarde" << setw(16) << "Vardas" << "Galutinis (Vid.)\n";
-    cout << string(46, '-') << "\n";
+    char pasirinkimas;
+    cout << "Vidurki (v), mediana (m) ar abu (a)? ";
+    cin >> pasirinkimas;
+
+    cout << left << setw(16) << "Pavarde" << setw(16) << "Vardas";
+    if (pasirinkimas == 'v')
+        cout << "Galutinis (Vid.)\n";
+    else if (pasirinkimas == 'm')
+        cout << "Galutinis (Med.)\n";
+    else
+        cout << "Galutinis (Vid.) / Galutinis (Med.)\n";
+
+    cout << string(60, '-') << "\n";
 
     cout << fixed << setprecision(2);
     for (studentas& s : studentai) {
-        cout << left << setw(16) << s.pav << setw(16) << s.var << s.rez << "\n";
+        cout << left << setw(16) << s.pav << setw(16) << s.var;
+        if (pasirinkimas == 'v')
+            cout << s.vid << "\n";
+        else if (pasirinkimas == 'm')
+            cout << s.med << "\n";
+        else
+            cout << setw(16) << s.vid << s.med << "\n";
     }
 
     return 0;
 }
+
